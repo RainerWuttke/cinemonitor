8f6152702d459f982604019a3627dff402a7b7c8
RainerWuttke <70168602+RainerWuttke@users.noreply.github.com>
2020-08-24 16:27:48 -0300

    Create cinemonitor.java

---
+++ cinemonitor.java
@@ -0,0 +1,32 @@
+public class Filme{
+  public static void main (String[] args) {
+    
+    int ano, tipo;
+    String titulo;
+    
+    public String getTitulo(){
+      return titulo;
+    }
+    
+    public int getAno(){
+      return titulo;
+    }
+    
+    public int getTipo(){
+      return titulo;
+    }
+    
+    public void setTitulo(String titulo){
+      this.titulo = titulo;
+    }
+    
+    public void setAno(int ano){
+      this.ano = ano;
+    }
+    
+    public void setTipo(int tipo){
+      this.tipo = tipo;
+    }
+    
+  }
+}
